97c4c315e88532a7c1763a5475168db8fc9e1454
Zixuan Feng <dl349799020@outlook.com>
2020-12-28 09:34:11 +0800

    A cmake test

---
+++ helloworld/CMakeFiles/power.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/power.dir/main.cc.obj"
+  "libpower.dll.a"
+  "power.exe"
+  "power.exe.manifest"
+  "power.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
